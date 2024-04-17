79d83da28450a6e4943c5137f0dfe571074e4f4c
WS-Ramon <ramon@webburo-spring.nl>
2024-04-17 15:55:36 +0200

    env test

--- test_git.php
+++ test_git.php
@@ -3,5 +3,8 @@ $servername = $_ENV['DB_SERVER'] ?? "localhost";
 $username = $_ENV['DB_USER'] ?? "lucas_koffie";
 $password = $_ENV['DB_PASSWORD'] ?? "4jmNqT5sEkVjTq2yW74s";
 $database = $_ENV['DB_DATABASE'] ?? 'lucas_koffie';
-
-var_dump($servername, $username, $password, $database);
+$getenvVars = [getenv("DB_SERVER"), getenv("DB_USER"), getenv("DB_PASSWORD"), getenv("DB_DATABASE")];
+echo "<pre>";
+var_dump($_ENV, $servername, $username, $password, $database);
+var_dump($getenvVars);
+echo "</pre>";
